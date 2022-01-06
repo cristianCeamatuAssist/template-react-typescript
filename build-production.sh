07a17d0e32eab5c65d3669b4647855c6c46a1dd8
Ceamatu Cristian <cristian.ceamatu@assist.ro>
2022-01-06 19:13:56 +0200

    Fix Husky Path

--- build-production.sh
+++ build-production.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export REACT_APP_CONTENT_URL=https://content.phoenix.virtualoutbound.com/
-export REACT_APP_user_pool=<get a secret key from somewhere using a CLI>
+export REACT_APP_user_pool="get a secret key from somewhere using a CLI"
 
 yarn run build
---
+++ src/components/Loaders/LoadingAnimation.tsx
@@ -0,0 +1,3 @@
+export const LoadingAnimation = () => {
+  return <div>Loading...</div>
+}
